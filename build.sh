0c605299ae3b32a80eae8ad83125480c0ba35434
n3shemmy3 <n3shemmy3@gmail.com>
2025-12-15 00:38:05 +0200

    added build.sh and some config files

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Kill the running instance (if any)
+flatpak kill dev.n3shemmy3.Screener || true
+
+# 2. Build and Install
+# If this fails, the script exits immediately (exit 1)
+flatpak-builder --force-clean --user --install build-dir dev.n3shemmy3.Screener.json || exit 1
+
+# 3. Run the new version
+flatpak run dev.n3shemmy3.Screener
