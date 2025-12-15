f8a3fe7e962dd44247e69dc6c8dfa9a52c9ace81
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2025-12-15 23:37:51 +0200

    bump up versions and use system config and build folders for faster builds

---
+++ flatpak-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Kill the running instance (if any)
+flatpak kill dev.n3shemmy3.Screener > /dev/null 2>&1 || true
+
+# 2. Build and Install
+# If this fails, the script exits immediately (exit 1)
+flatpak-builder --ccache --force-clean --user --install build-dir dev.n3shemmy3.Screener.json || exit 1
+
+# 3. Run the new version
+flatpak run dev.n3shemmy3.Screener
